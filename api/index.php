50e43e9ebd09bfecf137e3fed17fe3754faadd20
SetyoFahmi <yofafanzi@gmail.com>
2026-03-05 09:56:25 +0700

    update commit

--- api/index.php
+++ api/index.php
@@ -9,7 +9,7 @@ $notulen = $stmt->fetchAll();
     <meta charset="UTF-8">
     <title>Daftar Notulen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="../notulen_app/static/css/style.css">
 </head>
 <body>
 <div class="container mt-4">
